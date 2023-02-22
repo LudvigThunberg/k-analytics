68bc40912aceb39e702f9b067f63cf8d3a204b0a
LudvigThunberg <ludvigthunberg@gmail.com>
2023-02-22 14:22:53 +0100

    feat: adds refreshTokenLogin+automatic GA report

--- dist/server.js
+++ dist/server.js
@@ -167,6 +167,101 @@ app.get("/google/analytics/", (req, res) => __awaiter(void 0, void 0, void 0, fu
         res.status(500).send(error);
     }
 }));
+app.get("/google/credentials", (req, res) => {
+    console.log("RQ.HEADERS", req.headers);
+    const { authorization, refresh, expiresIn } = req.headers;
+    // Save to DB here
+    res.send(200);
+});
+app.get("/google/get-data", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const assessToken = process.env.GOOGLE_ACCESS_TOKEN;
+    const expiresIn = parseInt(process.env.GOOGLE_EXPIRES_IN);
+    const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
+    if (Date.now() > expiresIn) {
+        console.log("Expired!!!!");
+        oauth2Client.setCredentials({
+            refresh_token: refreshToken,
+        });
+    }
+    else {
+        oauth2Client.setCredentials({ access_token: assessToken });
+    }
+    console.log("Date.now:", Date.now());
+    console.log("EXPIRES_IN:", expiresIn);
+    try {
+        const queryData = yield analyticsData.properties.runReport({
+            property: process.env.GOOGLE_PROPERTY_ID,
+            requestBody: {
+                dateRanges: [
+                    {
+                        startDate: "2023-02-08",
+                        endDate: "2023-02-09",
+                    },
+                ],
+                dimensions: [
+                    {
+                        name: "city",
+                    },
+                ],
+                metrics: [
+                    {
+                        name: "totalUsers",
+                    },
+                ],
+            },
+        });
+        console.log("queryData", queryData.data);
+        res.status(200).send(queryData.data);
+    }
+    catch (error) {
+        console.log("ERRERROR: ", error);
+        res.status(500).send(error);
+    }
+}));
+const getData = () => __awaiter(void 0, void 0, void 0, function* () {
+    const assessToken = process.env.GOOGLE_ACCESS_TOKEN;
+    const expiresIn = parseInt(process.env.GOOGLE_EXPIRES_IN);
+    const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
+    if (Date.now() > expiresIn) {
+        console.log("Expired!!!!");
+        oauth2Client.setCredentials({
+            refresh_token: refreshToken,
+        });
+    }
+    else {
+        oauth2Client.setCredentials({ access_token: assessToken });
+    }
+    try {
+        const queryData = yield analyticsData.properties.runReport({
+            property: process.env.GOOGLE_PROPERTY_ID,
+            requestBody: {
+                dateRanges: [
+                    {
+                        startDate: "2023-02-08",
+                        endDate: "2023-02-09",
+                    },
+                ],
+                dimensions: [
+                    {
+                        name: "city",
+                    },
+                ],
+                metrics: [
+                    {
+                        name: "totalUsers",
+                    },
+                ],
+            },
+        });
+        // save to DB here
+        console.log("queryData", queryData.data);
+    }
+    catch (error) {
+        console.log("ERRERROR: ", error);
+    }
+});
+// setInterval(getData, 60000);
+app.get;
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
